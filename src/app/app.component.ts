55b401e502e63f7260eb6543569e44fc1130e874
Jack <jackkong@bu.edu>
2025-09-13 03:10:46 -0400

    added customize section partially

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroExplodeComponent } from './hero-explode/hero-explode.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroExplodeComponent, HeaderComponent],
+  imports: [HeaderComponent, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HeroExplodeComponent } from './hero-explode/hero-explode.component';
+import { FaceDeckStepComponent } from './customize/face-deck-step/face-deck-step.component';
+import { CustomizerStepComponent } from './customize/customizer-step/customizer-step.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroExplodeComponent },
+  { path: 'putters', component: HeroExplodeComponent }, // temp
+  { path: 'app', component: HeroExplodeComponent },      // temp
+  { path: 'about', component: HeroExplodeComponent },    // temp
+  // customizer
+  {
+    path: 'customize',
+    children: [
+        { path: ':step', component: CustomizerStepComponent }, 
+        // { path: 'face-deck', component: FaceDeckStepComponent },      // future steps:
+      // { path: 'connector', loadComponent: ... },
+      // { path: 'weighting', loadComponent: ... },
+      { path: '', pathMatch: 'full', redirectTo: 'face' },
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
